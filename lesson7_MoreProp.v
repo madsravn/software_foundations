cb9d8d61722d045b0d8f36aefe8204a1027c08a3
Emma Tosch <etosch@gmail.com>
2013-09-01 01:11:40 -0400

    need to think about fib in the other direction

--- lesson7_MoreProp.v
+++ lesson7_MoreProp.v
@@ -176,119 +176,124 @@ Module R.
   We can define three-place relations, four-place relations, etc., in just the same way as binary relations. For example, consider the following three-place relation on numbers:
 **)
 
-Inductive R : nat → nat → nat → Prop :=
+Inductive R : nat -> nat -> nat -> Prop :=
    | c1 : R 0 0 0 
-   | c2 : ∀m n o, R m n o → R (S m) n (S o)
-   | c3 : ∀m n o, R m n o → R m (S n) (S o)
-   | c4 : ∀m n o, R (S m) (S n) (S (S o)) → R m n o
-   | c5 : ∀m n o, R m n o → R n m o.
+   | c2 : forall m n o, R m n o -> R (S m) n (S o)
+   | c3 : forall m n o, R m n o -> R m (S n) (S o)
+   | c4 : forall m n o, R (S m) (S n) (S (S o)) -> R m n o
+   | c5 : forall m n o, R m n o -> R n m o.
 
-Which of the following propositions are provable?
-R 1 1 2
-R 2 2 6
-If we dropped constructor c5 from the definition of R, would the set of provable propositions change? Briefly (1 sentence) explain your answer.
-If we dropped constructor c4 from the definition of R, would the set of provable propositions change? Briefly (1 sentence) explain your answer.
-(* FILL IN HERE *)
-☐
+
+(** Which of the following propositions are provable? **)
+Example one : (R 1 1 2).
+Proof. apply c2. apply c3. apply c1. Qed.
+(** 
+Example two : (R 2 2 6).
+Proof. apply c2. apply c2. apply c3. apply c3. ...
+Not provable.. **)
+
+(** If we dropped constructor c5 from the definition of R, would the set of provable propositions change? **)
+(** No **)
+
+(** Briefly (1 sentence) explain your answer. **)
+(** c5 gives us no progress; it gives us back what we put in, so we will never choose it as part of proof anyway **)
+
+(** If we dropped constructor c4 from the definition of R, would the set of provable propositions change? Briefly (1 sentence) explain your answer. **)
+(** No; c4 is the inverse of the composition of c2 and c3. Since it does not reduce the answer to c1 and since we can exactly undo its application through c2 and c3, we never use it. *)
+
+
+
+(**************************************************
 Exercise: 3 stars, optional (R_fact)
 Relation R actually encodes a familiar function. State and prove two theorems that formally connects the relation and the function. That is, if R m n o is true, what can we say about m, n, and o, and vice versa?
+**************************************************)
+Example fact_or_fib_4 : (R 1 2 3).
+Proof. 
+  apply c2. 
+  apply c3. apply c3. 
+  apply c1. Qed.
+Example fact_or_fib_5 : (R 2 3 5).
+Proof. 
+  apply c2. apply c2. 
+  apply c3. apply c3. apply c3. 
+  apply c1. Qed.
+Example fact_or_fib_6 : (R 3 5 8).
+Proof. 
+  apply c2. apply c2. apply c2.
+  apply c3. apply c3. apply c3. apply c3. apply c3.
+  apply c1. Qed.
+(* Clearly this models the fibonnaci sequence, not factorial, as the name would suggest *)
 
-(* FILL IN HERE *)
-☐
+Theorem pred_S : forall m n,
+                   S m = S n -> pred (S m) = pred (S n).
+Proof. 
+  intros m n H.
+  destruct  m as [|mm].  destruct n as [|nn].
+  reflexivity.
+  rewrite H. reflexivity.
+  rewrite H. reflexivity.
+Qed.
+
+Theorem strip_S : forall m n,
+                    S m = S n ->  m = n.
+Proof.
+  intros n m. generalize dependent n. induction m as [|mm].
+  intros n H. destruct n. 
+  reflexivity. inversion H.
+  intros n H. induction n as [|nn].
+  inversion H.
+  apply pred_S in H. simpl in H.
+  apply pred_S in H. simpl in H.
+  rewrite H. reflexivity.
+Qed.
+
+Theorem R_is_fib : forall m n o,
+                     R m n o -> m + n = o.
+Proof. 
+  intros m n o H.
+  induction H.
+  simpl. reflexivity.
+  rewrite plus_Sn_m. rewrite IHR. reflexivity.
+  rewrite <- plus_n_Sm. rewrite IHR. reflexivity.
+  rewrite <- plus_n_Sm in IHR. rewrite plus_Sn_m in IHR.
+  apply strip_S in IHR.
+  apply strip_S in IHR.
+  apply IHR.
+  rewrite plus_comm.
+  apply IHR.
+Qed.
+
+Fixpoint fib i : nat :=
+match i with
+  | 0 => 0
+  | 1 => 1
+  | S n => fib n + fib (pred n)
+end.
+
+Example fib_ex : (fib 2 = 1).
+Proof. simpl. reflexivity. Qed.
+Example fib_ex_3 : (fib 3 = 2).
+Proof. simpl. reflexivity. Qed.
+Example fib_ex_4 : (fib 4 = 3).
+Proof. simpl. reflexivity. Qed.
+Example fib_ex_5 : (fib 5 = 5).
+Proof. simpl. reflexivity. Qed.
+Example fib_ex_6 : (fib 6 = 8).
+Proof. simpl. reflexivity. Qed.
+
+Theorem fib_is_R : forall x y,
+                     x >= 2 -> fib x = y -> R (fib (pred (pred x))) (fib (pred x)) y.
+Proof. 
+  intros x y E1 E2.
+  inversion E1.
+  rewrite <- H in E2.
+  simpl in E2.
+  simpl. rewrite <- E2.
+  apply c3. apply c1.
+  simpl.
+  rewrite <- E2.
+  rewrite <- H0.
+  simpl.Admitted.
+
 
 End R.
-
-
-Exercise: 2 stars (total_relation)
-Define an inductive binary relation total_relation that holds between every pair of natural numbers.
-
-(* FILL IN HERE *)
-☐
-Exercise: 2 stars (empty_relation)
-Define an inductive binary relation empty_relation (on numbers) that never holds.
-
-(* FILL IN HERE *)
-☐
-Exercise: 2 stars, optional (le_exercises)
-Here are a number of facts about the <= and < relations that we are going to need later in the course. The proofs make good practice exercises.
-
-Lemma le_trans : ∀m n o, m <= n → n <= o → m <= o.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem O_le_n : ∀n,
-  0 <= n.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem n_le_m__Sn_le_Sm : ∀n m,
-  n <= m → S n <= S m.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem Sn_le_Sm__n_le_m : ∀n m,
-  S n <= S m → n <= m.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem le_plus_l : ∀a b,
-  a <= a + b.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem plus_lt : ∀n1 n2 m,
-  n1 + n2 < m →
-  n1 < m ∧ n2 < m.
-Proof.
- unfold lt.
- (* FILL IN HERE *) Admitted.
-
-Theorem lt_S : ∀n m,
-  n < m →
-  n < S m.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem ble_nat_true : ∀n m,
-  ble_nat n m = true → n <= m.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem le_ble_nat : ∀n m,
-  n <= m →
-  ble_nat n m = true.
-Proof.
-  (* Hint: This may be easiest to prove by induction on m. *)
-  (* FILL IN HERE *) Admitted.
-
-Theorem ble_nat_true_trans : ∀n m o,
-  ble_nat n m = true → ble_nat m o = true → ble_nat n o = true.
-Proof.
-  (* Hint: This theorem can be easily proved without using induction. *)
-  (* FILL IN HERE *) Admitted.
-
-Exercise: 3 stars (R_provability)
-Module R.
-We can define three-place relations, four-place relations, etc., in just the same way as binary relations. For example, consider the following three-place relation on numbers:
-
-Inductive R : nat → nat → nat → Prop :=
-   | c1 : R 0 0 0 
-   | c2 : ∀m n o, R m n o → R (S m) n (S o)
-   | c3 : ∀m n o, R m n o → R m (S n) (S o)
-   | c4 : ∀m n o, R (S m) (S n) (S (S o)) → R m n o
-   | c5 : ∀m n o, R m n o → R n m o.
-
-Which of the following propositions are provable?
-R 1 1 2
-R 2 2 6
-If we dropped constructor c5 from the definition of R, would the set of provable propositions change? Briefly (1 sentence) explain your answer.
-If we dropped constructor c4 from the definition of R, would the set of provable propositions change? Briefly (1 sentence) explain your answer.
-(* FILL IN HERE *)
-☐
-Exercise: 3 stars, optional (R_fact)
-Relation R actually encodes a familiar function. State and prove two theorems that formally connects the relation and the function. That is, if R m n o is true, what can we say about m, n, and o, and vice versa?
-
-(* FILL IN HERE *)
-☐
-
-End R. 
